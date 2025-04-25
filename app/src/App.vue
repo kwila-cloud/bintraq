4404889dc0edefd302c5c9df2f85a69d8da055e5
Addison Emig <pertemptodisce@gmail.com>
2025-04-25 01:11:04 -0400

    feat: add basic supabase app (#1)
    * feat: add vue app
    
    * feat: display list of bins
    
    * feat: order by date
    
    * feat: remove redirects
    
    * fix: use TS in App.vue
    
    * chore: debug env
    
    * feat: prettify index.html
    
    * chore: remove unneeded files
    
    * feat: use multiple pages

---
+++ app/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <h1>BinTraq</h1>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
+</template>
---
+++ app/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>About BinTraq</h1>
+  <p><i>A simple bin record keeping system for orchards.</i></p>
+  <p>
+    Developed by <a href="https://kwila.cloud">Kwila Cloud</a> for
+    <a href="https://traesureappleranch.com">Traesure Apple Ranch.</a>
+  </p>
+  <p>
+    For more information, see our <a href="https://github.com/kwila-cloud/bintraq">GitHub page</a>.
+  </p>
+</template>
+
+<style>
+p {
+  padding: 8px 0;
+}
+</style>
