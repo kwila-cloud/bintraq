709af7748d98e059e2ec03ff687ebbd35f73a295
Addison Emig <addison@kwila.cloud>
2025-05-01 20:21:33 -0400

    feat: add tailwind

--- app/src/views/BinsView.vue
+++ app/src/views/BinsView.vue
@@ -1,30 +1,35 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import { ref, onMounted } from 'vue'
-import { useRouter } from 'vue-router'
+import { supabase } from "@/lib/supabaseClient";
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
 
 type Bin = {
-  uuid: string
-  id: string
-  picker: string
-}
+  uuid: string;
+  id: string;
+  picker: string;
+};
 
-const bins = ref<Bin[]>([])
+const bins = ref<Bin[]>([]);
 
 async function getBins() {
-  const { data } = await supabase.from('bin').select().order('date')
-  bins.value = data as Bin[]
+  const { data } = await supabase.from("bin").select().order("date");
+  bins.value = data as Bin[];
 }
 
 onMounted(() => {
-  getBins()
-})
+  getBins();
+});
 </script>
 
 <template>
-  <h1>Bins</h1>
+  <div class="flex flex-row gap-4 items-center">
+    <h1>Bins</h1>
+    <button>Add</button>
+  </div>
   <ul>
-    <li v-for="bin in bins" :key="bin.uuid">{{ bin.id }} (picked by {{ bin.picker }})</li>
+    <li v-for="bin in bins" :key="bin.uuid">
+      {{ bin.id }} (picked by {{ bin.picker }})
+    </li>
   </ul>
   <p style="padding-top: 16px">Authentication guard is now active.</p>
 </template>
