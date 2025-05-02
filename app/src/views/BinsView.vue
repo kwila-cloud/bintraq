b42bb5cba9ad6810fe76a8730aada39b3c01dfa0
Addison Emig <addison@kwila.cloud>
2025-05-01 20:40:35 -0400

    feat: start on AddBinDialog

--- app/src/views/BinsView.vue
+++ app/src/views/BinsView.vue
@@ -2,6 +2,7 @@
 import { supabase } from "@/lib/supabaseClient";
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+import AddBinDialog from "@/components/AddBinDialog.vue";
 
 type Bin = {
   uuid: string;
@@ -24,12 +25,11 @@ onMounted(() => {
 <template>
   <div class="flex flex-row gap-4 items-center">
     <h1>Bins</h1>
-    <button>Add</button>
+    <AddBinDialog />
   </div>
   <ul>
     <li v-for="bin in bins" :key="bin.uuid">
       {{ bin.id }} (picked by {{ bin.picker }})
     </li>
   </ul>
-  <p style="padding-top: 16px">Authentication guard is now active.</p>
 </template>
