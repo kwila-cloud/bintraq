07243c6409b5316638235bad346ddee3753abaa5
Addison Emig <addison@kwila.cloud>
2025-05-23 13:11:55 -0400

    feat: add bin (#10)
    * feat: add tailwind
    
    * feat: start on AddBinDialog
    
    * feat: improve styling
    
    * refactor: Update login page to use Tailwind CSS
    
    * clean up
    
    * feat: start on AddBinView
    
    * feat: Implement BinSetting component for editing bin properties
    
    * feat: improve styling
    
    * feat: Close dialog on click outside the dialog container
    
    * feat: more styling improvements
    
    * feat: actually add a bin

---
+++ app/src/views/AddBinView.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import type { Bin } from "@/models/bin";
+import { blockOptions, sizeOptions } from "@/models/bin";
+import { onMounted, ref } from "vue";
+import BinSetting from "@/components/BinSetting.vue";
+import { supabase } from "@/lib/supabaseClient";
+
+const pendingBin = ref<Partial<Bin>>({
+  picker: "",
+  block: "",
+  size: "",
+  id: "",
+  isPending: true,
+  messageUuid: null,
+});
+// TODO: load pickers from supabase
+const pickers = ref(["Addison", "Milo"]);
+const blocks = ref(blockOptions);
+const sizes = ref(sizeOptions);
+const settings = ref([
+  { id: "picker", name: "Picker", options: pickers, type: "select" },
+  { id: "block", name: "Block", options: blocks, type: "select" },
+  { id: "size", name: "Bin Size", options: sizes, type: "select" },
+  { id: "id", name: "Bin ID", type: "text" },
+]);
+const bins = ref<Bin[]>([]);
+
+async function getBins() {
+  const { data } = await supabase
+    .from("bin")
+    .select()
+    .order("date", { ascending: false });
+  bins.value = data as Bin[];
+  // Use the most recent bin as default values
+  if (bins.value.length > 0) {
+    pendingBin.value.picker = bins.value[0].picker;
+    pendingBin.value.block = bins.value[0].block;
+    pendingBin.value.size = bins.value[0].size;
+  }
+}
+
+onMounted(() => {
+  getBins();
+});
+
+const addBin = async () => {
+  for (const setting of settings.value) {
+    if (!pendingBin.value[setting.id]) {
+      // TODO: use prettier alert
+      alert(`Please select a ${setting.name}`);
+      return;
+    }
+  }
+  if (!validateBinId(pendingBin.value.id ?? "")) {
+    return;
+  }
+  // Add the new bin
+  await supabase.from("bin").insert(pendingBin.value);
+  // Clear the selected bin ID
+  pendingBin.value.id = "";
+  // Refresh the list of bins
+  getBins();
+};
+
+const validateBinId = (binId: string) => {
+  if (binId.length != 4) {
+    alert(`The bin ID must be 4 characters long.`);
+    return false;
+  }
+  const allBins = [...bins.value];
+  const match = allBins.find(({ id }) => id == binId);
+  if (match) {
+    alert(`The bin ID "${binId}" has already been added.`);
+    return false;
+  }
+  return true;
+};
+</script>
+
+<template>
+  <h1>Add Bin</h1>
+  <div class="flex flex-col gap-2">
+    <div v-for="setting in settings" :key="setting.id">
+      <BinSetting :setting="setting" v-model="pendingBin[setting.id]" />
+    </div>
+    <button @click="addBin" class="bg-blue-800 rounded-md p-2">Add</button>
+  </div>
+</template>
--- app/src/views/BinsView.vue
+++ app/src/views/BinsView.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { supabase } from "@/lib/supabaseClient";
 import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
-import AddBinDialog from "@/components/AddBinDialog.vue";
 
 type Bin = {
   uuid: string;
@@ -23,10 +21,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-row gap-4 items-center">
-    <h1>Bins</h1>
-    <AddBinDialog />
-  </div>
+  <h1>Bins</h1>
   <ul>
     <li v-for="bin in bins" :key="bin.uuid">
       {{ bin.id }} (picked by {{ bin.picker }})
