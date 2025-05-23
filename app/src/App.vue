587042b6812bf9678ef98ada1dc2412959bf14d9
Addison Emig <addison@kwila.cloud>
2025-05-23 13:18:58 -0400

    feat: add bottom nav bar

--- app/src/App.vue
+++ app/src/App.vue
@@ -28,14 +28,10 @@ async function signOut() {
 </script>
 
 <template>
-  <nav>
+  <nav id="top-nav">
     <RouterLink v-if="isLoggedIn" to="/bins">BinTraq</RouterLink>
     <RouterLink v-else to="/about">BinTraq</RouterLink>
-
-    <RouterLink v-if="isLoggedIn" to="/bins">Bins</RouterLink>
-    <RouterLink v-if="isLoggedIn" to="/bins/add">Add Bin</RouterLink>
     <RouterLink to="/about">About</RouterLink>
-
     <RouterLink v-if="!isLoggedIn" to="/login">Login</RouterLink>
     <button v-else @click="signOut" class="button-as-a">Logout</button>
   </nav>
@@ -43,21 +39,45 @@ async function signOut() {
     <RouterView />
   </main>
 
-  <nav v-if="isLoggedIn" class="bottom-nav">
-    <RouterLink to="/bins">Bins</RouterLink>
+  <nav v-if="isLoggedIn" id="bottom-nav">
     <RouterLink to="/bins/add">Add Bin</RouterLink>
+    <RouterLink to="/bins">Bins</RouterLink>
   </nav>
 </template>
 
 <style scoped>
-.bottom-nav {
+#top-nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  background: var(--color-slate-800);
+  padding: 8px 12px;
+  border-radius: 12px;
+
+  :first-child {
+    margin-inline-end: auto;
+    font-weight: bold;
+    font-size: 1.5rem;
+    color: var(--color-white);
+  }
+
+  :not(:first-child) {
+    font-size: 1rem;
+    line-height: 2;
+    padding: 8px;
+    background: var(--color-slate-700);
+    border-radius: 4px;
+  }
+}
+#bottom-nav {
   position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
-  background-color: #f0f0f0;
   padding: 10px;
   display: flex;
   justify-content: space-around;
+  background: var(--color-slate-800);
 }
 </style>
