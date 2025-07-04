e86512d5064e35ee9b2f76965ff4830b69d3900a
Addison Emig <addison@kwila.cloud>
2025-07-04 11:08:37 -0400

    feat: cards in history view

--- app/src/views/HistoryView.vue
+++ app/src/views/HistoryView.vue
@@ -1,33 +1,76 @@
 <script setup lang="ts">
-import { supabase } from "@/lib/supabaseClient";
-import { ref, onMounted } from "vue";
+import { supabase } from '@/lib/supabaseClient'
+import type { Bin } from '@/models/bin'
+import { ref, onMounted } from 'vue'
 
-type Bin = {
-  uuid: string;
-  id: string;
-  picker: string;
-};
-
-const bins = ref<Bin[]>([]);
+const bins = ref<Bin[]>([])
+const messageStatuses = ref<Record<string, string>>({})
 
 async function loadCompletedBins() {
   const { data } = await supabase
-    .from("bin")
+    .from('bin')
     .select()
-    .eq("isPending", false)
-    .order("date");
-  bins.value = data as Bin[];
+    .eq('isPending', false)
+    .order('date', { ascending: false })
+  bins.value = data as Bin[]
+}
+
+async function loadMessageStatuses() {
+  // TODO: load from smoketree
+}
+
+async function resend(messageUuid: string) {
+  // TODO: resend message with smoketree
 }
 
 onMounted(() => {
-  loadCompletedBins();
-});
+  loadCompletedBins()
+  loadMessageStatuses()
+})
 </script>
 
 <template>
   <ul>
     <li v-for="bin in bins" :key="bin.uuid">
-      {{ bin.id }} (picked by {{ bin.picker }})
+      <h1>{{ bin.id }}</h1>
+      <span>{{ bin.picker }}</span>
+      <span>{{ bin.date.toLocaleString() }}</span>
+      <span v-if="bin.messageUuid == null || messageStatuses[bin.messageUuid] == undefined"
+        >Bin UUID - {{ bin.uuid }}</span
+      >
+      <template v-if="bin.messageUuid != null">
+        <span>Send Status - {{ messageStatuses[bin.messageUuid] ?? 'unknown' }}</span>
+        <button
+          v-if="messageStatuses[bin.messageUuid] == 'failed'"
+          @click="resend(bin.messageUuid)"
+        >
+          Resend
+        </button>
+      </template>
     </li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+li {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 8px;
+  border-radius: 8px;
+  background-color: var(--color-slate-800);
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  button {
+    background-color: var(--color-blue-900);
+  }
+}
+</style>
