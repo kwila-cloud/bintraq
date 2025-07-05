0b20601ffbc7481c7fe735d159c762f6e3ded490
Addison Emig <addison@kwila.cloud>
2025-07-05 15:22:57 -0400

    Revert "feat: Implement drag-and-drop picker reordering"
    This reverts commit 7691c1aac017fae20fa03acb26d0f0982758f638.

--- app/src/components/MenuModal.vue
+++ app/src/components/MenuModal.vue
@@ -6,33 +6,12 @@ const props = defineProps({
     type: String,
     default: 'Menu',
   },
-  draggable: {
-    type: Boolean,
-    default: false,
-  },
 })
 
 const emit = defineEmits(['close'])
 
 function closeModal() {
   emit('close')
-}
-
-// Drag and drop handlers to be passed to the slot
-function dragStart(event: DragEvent, index: number) {
-  event.dataTransfer?.setData('text/plain', index.toString())
-  event.dataTransfer!.effectAllowed = 'move'
-}
-
-function dragOver(event: DragEvent, index: number) {
-  event.preventDefault()
-  event.dataTransfer!.dropEffect = 'move'
-}
-
-function drop(event: DragEvent, index: number) {
-  event.preventDefault()
-  const draggedIndex = parseInt(event.dataTransfer!.getData('text/plain'))
-  emit('drop', { draggedIndex, targetIndex: index })
 }
 </script>
 
@@ -43,11 +22,8 @@ function drop(event: DragEvent, index: number) {
   >
     <div class="bg-slate-800 p-5 rounded-lg relative min-w-[250px] shadow-lg">
       <h1 class="text-xl font-bold mb-4 text-center">{{ props.title }}</h1>
-      <div class="flex flex-col gap-2">
-        <slot :dragStart="dragStart" :dragOver="dragOver" :drop="drop"></slot>
-      </div>
-      <div v-if="$slots.footer" class="mt-4 pt-4 border-t border-slate-700">
-        <slot name="footer"></slot>
+      <div class="flex flex-col gap-2" @click="closeModal">
+        <slot></slot>
       </div>
     </div>
   </div>
