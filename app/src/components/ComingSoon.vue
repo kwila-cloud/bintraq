b3497317bc12f191525d27cc56d0d47bff3545f6
Addison Emig <addison@kwila.cloud>
2025-07-05 11:52:12 -0400

    feat: ComingSoon component

---
+++ app/src/components/ComingSoon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-full">
+    <Icon icon="system-uicons:hourglass-half" height="64" class="mb-4 text-gray-400" />
+    <h1 class="text-3xl font-bold text-gray-300 mb-2">Coming Soon!</h1>
+    <p class="text-lg text-gray-400">This page is under development and will be available soon.</p>
+  </div>
+</template>
+
+<style scoped>
+/* Add any specific styles here if needed */
+</style>
--- app/src/views/ManagePickersView.vue
+++ app/src/views/ManagePickersView.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { Icon } from '@iconify/vue'
+import ComingSoon from '@/components/ComingSoon.vue'
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-full">
-    <Icon icon="system-uicons:hourglass-half" height="64" class="mb-4 text-gray-400" />
-    <h1 class="text-3xl font-bold text-gray-300 mb-2">Coming Soon!</h1>
-    <p class="text-lg text-gray-400">This page is under development and will be available soon.</p>
-  </div>
+  <ComingSoon />
 </template>
 
 <style scoped>
