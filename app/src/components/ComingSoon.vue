00544c085b60ac9d6fa6e45b74b15e39ccdb42a1
Addison Emig <addison@kwila.cloud>
2025-07-05 11:59:41 -0400

    fix: icon

--- app/src/components/ComingSoon.vue
+++ app/src/components/ComingSoon.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue'
 
 <template>
   <div class="flex flex-col items-center justify-center h-full">
-    <Icon icon="system-uicons:hourglass-half" height="64" class="mb-4 text-gray-400" />
+    <Icon icon="system-uicons:graph-increase" height="64" class="mb-4 text-gray-400" />
     <h1 class="text-3xl font-bold text-gray-300 mb-2">Coming Soon!</h1>
     <p class="text-lg text-gray-400">This page is under development and will be available soon.</p>
   </div>
